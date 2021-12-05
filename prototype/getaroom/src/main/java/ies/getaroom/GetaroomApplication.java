ff8b50c33de5b8adb338982ed0bf77992bf24907
tomascandeias <tomaslcandeias@outlook.com>
2021-12-05 16:18:35 +0000

    search for a room base

---
+++ prototype/getaroom/src/main/java/ies/getaroom/GetaroomApplication.java
@@ -0,0 +1,13 @@
+package ies.getaroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetaroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetaroomApplication.class, args);
+	}
+
+}
---
+++ prototype/getaroom/src/test/java/ies/getaroom/GetaroomApplicationTests.java
@@ -0,0 +1,13 @@
+package ies.getaroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetaroomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
