8b93e7b6f821a753fd6b05f7cc09baf52cfa16b8
Martinho Tavares <martinho.tavares@ua.pt>
2021-12-11 13:12:04 +0000

    Created broker client spring app, successfully established communication

--- RaspberryPi/mosquitto/main.cpp
+++ RaspberryPi/mosquitto/main.cpp
@@ -3,10 +3,11 @@
 #include <csignal>
 #include <algorithm>
 #include <iterator>
+#include <cstring>
 
 const char* client_name = "melga";
 
-const char* broker_host = "192.168.1.66";
+const char* broker_host = "localhost";
 // It's usually 1883
 const int broker_port = 1883;
 // The number of seconds after which the broker should send a PING message to the client if no other messages have been exchanged in that time
@@ -28,7 +29,7 @@ void msg_received(struct mosquitto* mosq, void* obj, const struct mosquitto_mess
 
 // TODO: not secure, maybe use TLS later?
 // This is a mosquitto instance that obtains data and sends it to the broker
-int main() {
+int main(int argc, char** argv) {
     struct mosquitto* client = mosquitto_new(client_name, true, nullptr);
     mosquitto_message_callback_set(client, msg_received);
 
@@ -36,8 +37,8 @@ int main() {
     if (status == MOSQ_ERR_SUCCESS) {
         int mid;
 
-        const char* msg = "Bom dia!";
-        mosquitto_publish(client, &mid, topic, 9, msg, qos, true);
+        const char* tomsg = argc>1 ? argv[1] : "Bom dia!{→↓←øŧæŋßþ“þ«»}";
+        mosquitto_publish(client, &mid, topic, strlen(tomsg), tomsg, qos, true);
 
         mosquitto_subscribe(client, &mid, topic, qos);
 
---
+++ broker/src/main/java/com/brokerclient/app/MQTTClient.java
@@ -0,0 +1,51 @@
+package com.brokerclient.app;
+
+import org.eclipse.paho.client.mqttv3.IMqttClient;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
+
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+@Component
+public class MQTTClient implements InitializingBean {
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        String name = "fetcher";
+        IMqttClient client;
+
+        System.out.println("Creating...");
+
+        client = new MqttClient("tcp://127.0.0.1:1883", name);
+
+        System.out.println("Connecting...");
+
+        MqttConnectOptions options = new MqttConnectOptions();
+        options.setAutomaticReconnect(true);
+        options.setCleanSession(false);
+        options.setConnectionTimeout(10);
+        client.connect(options);
+
+        CountDownLatch receivedMsg = new CountDownLatch(10);
+        client.subscribe("mosquitto/test", (topic, msg) -> {
+            byte[] payload = msg.getPayload();
+            String msgStr = byteArrayDecode(payload);
+            System.out.println("Message: " + msgStr);
+            receivedMsg.countDown();
+        });
+        receivedMsg.await(1, TimeUnit.MINUTES);
+    }
+
+    private String byteArrayDecode(byte[] bytes) {
+        Charset charset = StandardCharsets.UTF_8;
+        return charset.decode(ByteBuffer.wrap(bytes)).toString();
+    }
+}
