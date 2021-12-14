ccacd70c984aed94be9e41cf8d462549ae7f7481
Miguel <mbeiraob@ua.pt>
2021-12-14 18:57:25 +0000

    Added sql script to initialize DB

---
+++ GetARoom/student-app/src/main/java/com/getaroom/app/controller/GetARoom.sql
@@ -0,0 +1,56 @@
+-- USE *database*;
+
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users(
+    username VARCHAR(20),
+    email VARCHAR(20) PRIMARY KEY,
+    password VARCHAR(20),
+    role VARCHAR(20)
+)
+
+INSERT INTO users
+VALUES ("Student" , "student@gmail.com" , SHA2('Password', 512), "student" );
+
+INSERT INTO users
+VALUES ("Security", "security@gmail.com", SHA2('Password', 512), "security");
+
+INSERT INTO users
+VALUES ("Analyst" , "analyst@gmail.com" , SHA2('Password', 512), "analyst" );
+
+DROP PROCEDURE IF EXISTS loggeIn;
+
+DELIMITER &&
+CREATE PROCEDURE loggeIn (IN username VARCHAR(20), IN password VARCHAR(255))
+    BEGIN
+        SELECT EXISTS(
+            SELECT * FROM users
+            WHERE users.username = username
+            AND  users.password = SHA2(password,512));
+    END &&  
+DELIMITER ;  
+
+-- CALL loggeIn("Student", "Password");
+-- CALL loggeIn("Student", "Passwor");
+
+DROP FUNCTION IF EXISTS register;
+
+DELIMITER &&
+CREATE FUNCTION register (username VARCHAR(20), email VARCHAR(20), password VARCHAR(20), role VARCHAR(20))
+    RETURNS VARCHAR(20)
+    DETERMINISTIC
+    BEGIN 
+        IF (EXISTS(
+            SELECT * FROM users
+            WHERE users.username = username
+            OR users.email = email
+        )) THEN RETURN 'Allready exists';
+        ELSE
+            INSERT INTO users VALUES (username, email, SHA2(password,512), role);
+            RETURN 'Register completed';
+        END IF;
+    END &&
+DELIMITER ; 
+
+-- SELECT register ("Student", "student@gmail", "Password", "Student");
+-- SELECT register ("Test", "test@gmail", "Password", "Student");
