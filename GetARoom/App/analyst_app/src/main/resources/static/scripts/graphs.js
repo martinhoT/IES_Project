33e09c40c291c0493ad40df9e1d70e65bba668a3
Miguel <mbeiraob@gmail.com>
2022-01-11 15:20:20 +0000

    Bugfix

--- GetARoom/App/analyst_app/src/main/resources/static/scripts/graphs.js
+++ GetARoom/App/analyst_app/src/main/resources/static/scripts/graphs.js
@@ -20,55 +20,58 @@ function drawChart() {
     viewModel.floors([]);
     var floorData = {};
     var roomsByFloor = {};
-    for(let event of Events){
-        var floor = event['room'].split(".").splice(0,2).join("-");
-
-        if(!(floor in roomsByFloor)){
-            roomsByFloor[floor] = {};
-        }
-        else if(Object.keys(roomsByFloor[floor]).length >= 15 && !(event['room'] in roomsByFloor[floor])){
-            floor = floor + "_2";
-            if(!(floor in roomsByFloor)){
-                roomsByFloor[floor] = {};
+    $.getJSON("http://localhost:84/api/status_history",
+        function (data, textStatus, jqXHR) {
+            for(let event of data){
+                var floor = event['room'].split(".").splice(0,2).join("-");
+            
+                if(!(floor in roomsByFloor)){
+                    roomsByFloor[floor] = {};
+                }
+                else if(Object.keys(roomsByFloor[floor]).length >= 15 && !(event['room'] in roomsByFloor[floor])){
+                    floor = floor + "_2";
+                    if(!(floor in roomsByFloor)){
+                        roomsByFloor[floor] = {};
+                    }
+                }
+            
+                if(!(floor in floorData)){
+                    viewModel.floors.push(floor);
+                    floorData[floor] = new google.visualization.DataTable();
+                    floorData[floor].addColumn('date', 'Time');
+                }
+            
+                if(!(event['room'] in roomsByFloor[floor])){
+                    roomsByFloor[floor][event['room']] = event['occupacy'];
+                    floorData[floor].addColumn('number', event['room']);
+                }
+            
+                var row = [new Date(event['time'])];
+            
+                for(var key in roomsByFloor[floor]){
+                    if(event['room'] === key){
+                        row.push(event['occupacy']);
+                        roomsByFloor[floor][event['room']] = event['occupacy'];
+                    }
+                    else{
+                        row.push(roomsByFloor[floor][key]);
+                    }
+                }
+            
+                floorData[floor].addRow(row);
             }
-        }
+        
+        
+            var options = {
+                title: 'Room occupacy by time',
+                curveType: 'function',
+                legend: {positon : 'bottom'},
+                vAxis: { format:'#,###%'}
+            };
 
-        if(!(floor in floorData)){
-            viewModel.floors.push(floor);
-            floorData[floor] = new google.visualization.DataTable();
-            floorData[floor].addColumn('date', 'Time');
-        }
-
-        if(!(event['room'] in roomsByFloor[floor])){
-            roomsByFloor[floor][event['room']] = event['occupacy'];
-            floorData[floor].addColumn('number', event['room']);
-        }
-
-        var row = [new Date(event['time'])];
-
-        for(var key in roomsByFloor[floor]){
-            if(event['room'] === key){
-                row.push(event['occupacy']);
-                roomsByFloor[floor][event['room']] = event['occupacy'];
+            for(var floor in floorData){
+                var chart = new google.charts.Line(document.getElementById('curve_chart_'+floor));
+                chart.draw(floorData[floor], google.charts.Line.convertOptions(options));
             }
-            else{
-                row.push(roomsByFloor[floor][key]);
-            }
-        }
-
-        floorData[floor].addRow(row);
-    }
-
-
-    var options = {
-        title: 'Room occupacy by time',
-        curveType: 'function',
-        legend: {positon : 'bottom'},
-        vAxis: { format:'#,###%'}
-    };
-    
-    for(var floor in floorData){
-        var chart = new google.charts.Line(document.getElementById('curve_chart_'+floor));
-        chart.draw(floorData[floor], google.charts.Line.convertOptions(options));
-    }
+    });
 }
