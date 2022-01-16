ef30fa0a080a48c6918de57d7a398021ad3338f7
Miguel <mbeiraob@gmail.com>
2022-01-16 17:17:30 +0000

    Dropdown completed

--- GetARoom/App/analyst_app/src/main/resources/static/scripts/graphs.js
+++ GetARoom/App/analyst_app/src/main/resources/static/scripts/graphs.js
@@ -2,25 +2,41 @@ google.charts.load('current', {'packages':['line']});
 google.charts.setOnLoadCallback(drawChart);
 
 var Events;
+var floorData = {};
+var options = {
+    title: 'Room occupacy by time',
+    curveType: 'function',
+    legend: {positon : 'bottom'},
+    vAxis: { format:'#,###%'}
+};
 
 function ViewModel(){
     var self = this;
     
     self.floors = ko.observableArray([]);
+    self.departments = ko.observableArray([]);
+    self.selectedFloor = ko.observable();
 }
 var viewModel = new ViewModel();
 ko.applyBindings(viewModel);
 
-$.getJSON("http://localhost:84/api/status_history",
+$.getJSON("http://localhost:84/api/status   ",
 function (data, textStatus, jqXHR) {
     Events = data;
 });
 
+$("#selected_floor").change(function() {
+    console.log( $("#selected_floor").val() );
+    viewModel.selectedFloor( $("#selected_floor").val() )
+    // TODO
+    var chart = new google.charts.Line(document.getElementById('curve_chart_'+$("#selected_floor").val()));
+    chart.draw(floorData[$("#selected_floor").val()], google.charts.Line.convertOptions(options));
+});
+
 function drawChart() {
     viewModel.floors([]);
-    var floorData = {};
     var roomsByFloor = {};
-    $.getJSON("http://localhost:84/api/status_history",
+    $.getJSON("http://localhost:84/api/status",
         function (data, textStatus, jqXHR) {
             for(let event of data){
                 var floor = event['room'].split(".").splice(0,2).join("-");
@@ -60,18 +76,6 @@ function drawChart() {
             
                 floorData[floor].addRow(row);
             }
-        
-        
-            var options = {
-                title: 'Room occupacy by time',
-                curveType: 'function',
-                legend: {positon : 'bottom'},
-                vAxis: { format:'#,###%'}
-            };
-
-            for(var floor in floorData){
-                var chart = new google.charts.Line(document.getElementById('curve_chart_'+floor));
-                chart.draw(floorData[floor], google.charts.Line.convertOptions(options));
-            }
+            
     });
 }
