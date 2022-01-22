a616cb4c97636b08e69c5ffa7c5cbfcb7758d2c3
Martinho Tavares <martinho.tavares@ua.pt>
2022-01-22 18:55:37 +0000

    Using Thymeleaf templates on all HTML, fixed student-app logout link, fixed formatting issue in the analyst-app's graphs page

--- GetARoom/App/secguard_app/src/main/java/com/getaroom/app/controller/MainController.java
+++ GetARoom/App/secguard_app/src/main/java/com/getaroom/app/controller/MainController.java
@@ -20,7 +20,6 @@ import org.springframework.ui.Model;
 import org.springframework.util.MultiValueMapAdapter;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.servlet.ModelAndView;
 
 import java.util.*;
 
