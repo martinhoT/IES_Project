4a70820726d933906fa3a592218d3fa6cae9b085
Pengrey <rodrigoflima@ua.pt>
2022-01-23 10:23:38 +0000

    Hard clean2

--- GetARoom/App/fetcher/src/main/java/com/getaroom/app/repository/mongodb/EventHistoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.getaroom.app.repository.mongodb;
-
-import com.getaroom.app.entity.mongodb.EventHistory;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface EventHistoryRepository extends MongoRepository<EventHistory, String> {}
--- GetARoom/App/fetcher/src/main/java/com/getaroom/app/repository/mysql/DepartmentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.getaroom.app.repository.mysql;
-
-import com.getaroom.app.entity.mysql.Department;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DepartmentRepository extends JpaRepository<Department, Integer> {}
